92592ecc718c0cdbbcf75fbc37c3d346a3ef99c7
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2022-03-02 20:13:54 +0100

    welcome page

--- hospitalproject/src/App.vue
+++ hospitalproject/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <welcomeSection />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import welcomeSection from './components/LandingPage/welcomeSection.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    welcomeSection
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ hospitalproject/src/components/LandingPage/welcomeSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="body">
+        <div class="logo"></div>
+        <div class="welcome">
+            <h2>Get well</h2>
+            <p>Whenever,Wherever</p>
+        </div>
+        <div class="login">
+            <p>Login as:</p>
+            <div class="admin"></div>
+            <div class="staff"></div>
+            <div class="doctor"></div>
+        </div>
+        
+    </div>
+</template>
